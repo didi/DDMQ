ab4107af3a9c3197fe084b6fe18eae11ced36d07
zanglei <zanglei@didichuxing.com>
2019-01-08 11:18:28 +0800

    update docker build.sh and add js resources

--- carrera-console/build.sh
+++ carrera-console/build.sh
@@ -4,11 +4,11 @@ BASEDIR=$(dirname "$0")
 cd ${BASEDIR}
 WKDIR=`pwd`
 
-cd ${BASEDIR}/carrera-console-fe
-sh build.sh
-cd ..
-rm -rf carrera-console/src/main/webapp/build carrera-console/src/main/webapp/static carrera-console/src/main/webapp/index.html
-cp -R carrera-console-fe/dist/* carrera-console/src/main/webapp
+# cd ${BASEDIR}/carrera-console-fe
+# sh build.sh
+# cd ..
+# rm -rf carrera-console/src/main/webapp/build carrera-console/src/main/webapp/static carrera-console/src/main/webapp/index.html
+# cp -R carrera-console-fe/dist/* carrera-console/src/main/webapp
 
 
 cd ${BASEDIR}/carrera-console
--- carrera-docker/build.sh
+++ carrera-docker/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+(cd ../carrera-common && mvn clean install -DskipTests)
+(cd ../carrera-sdk/consumer/java/carrera-consumer-sdk && mvn clean install -DskipTests)
+(cd ../carrera-sdk/producer/java/carrera-producer-sdk && mvn clean install -DskipTests)
+
 ../carrera-producer/build.sh && cp ../carrera-producer/target/carrera-producer-1.0.0-SNAPSHOT-jar-with-dependencies.jar producer/
 ../carrera-consumer/build.sh && cp ../carrera-consumer/target/carrera-consumer-1.0.0-SNAPSHOT-jar-with-dependencies.jar consumer/
 ../carrera-chronos/build.sh && cp ../carrera-chronos/target/chronos-1.0.0-SNAPSHOT-jar-with-dependencies.jar chronos/
